9e3ef42c01ee6c74c1c6ae189a0343fd1556d08c
chapetta <cha.petta@hotmail.com>
2025-09-29 16:36:15 -0300

    refact: refatorando o arquivo server

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ const port = 3000;
 const app = express();
 
 
-app.get("/", (req, res) => {
-  res.send("Home page");
+app.get("/movies", (req, res) => {
+  res.send("Listagem de filmes");
 });
 
 app.listen(port, () => {
